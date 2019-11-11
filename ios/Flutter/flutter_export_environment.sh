0cf07c04d1436ef30bb813658d5aa5fadab9eeec
Thalhah Shafiyurrahman <binfachri@gmail.com>
2019-11-11 14:37:16 +0700

    Initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/thalhah/Library/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/thalhah/Downloads/flutter_countdown_timer-master"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/thalhah/Library/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
